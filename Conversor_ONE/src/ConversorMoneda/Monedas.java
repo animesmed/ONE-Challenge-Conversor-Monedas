8085f8040288448773e303d744532ff7c7ae74b0
animesmed <k.animesmed27@gmail.com>
2023-08-10 13:16:35 -0500

    Creando el valor de cada transformación

--- Conversor_ONE/src/ConversorMoneda/Monedas.java
+++ Conversor_ONE/src/ConversorMoneda/Monedas.java
@@ -8,42 +8,25 @@ public class Monedas {
   	- Convertir de la moneda de tu país  a Yen Japonés
   	- Convertir de la moneda de tu país  a Won sul-coreano
     */
-	public double ConvertirSolADolar(double val) {
-		return val;
-	}
-	public double ConvertirSolAEuro(double val) {
-		return val;
-	}
-	public double ConvertirSolALibrasE(double val) {
-		return val;
-	}
-	public double ConvertirSolAYen(double val) {
-		return val;
-	}
-	public double ConvertirSolAWon(double val) {
-		return val;
-	}
+	public double DolarASol = 3.70;
+	public double EuroASol = 4.06;
+	public double LibrasEASol = 4.69;
+	public double YenASol = 0.026;
+	public double WonASol = 0.0028;
+	
 	//=======
-	/*
-	- Convertir de Dólar a la moneda de tu país
-    - Convertir de Euros a la moneda de tu país
-    - Convertir de Libras Esterlinas a la moneda de tu país
-    - Convertir de Yen Japonés a la moneda de tu país
-    - Convertir de Won sul-coreano a la moneda de tu país
-	*/
-	public double ConvertirDolarASol(double val) {
-		return val;
-	}
-	public double ConvertirEuroASol(double val) {
-		return val;
-	}
-	public double ConvertirLibrasASol(double val) {
-		return val;
-	}
-	public double ConvertirYenASol(double val) {
-		return val;
-	}
-	public double ConvertirWonASol(double val) {
-		return val;
-	}
+		/*
+		- Convertir de Dólar a la moneda de tu país
+	    - Convertir de Euros a la moneda de tu país
+	    - Convertir de Libras Esterlinas a la moneda de tu país
+	    - Convertir de Yen Japonés a la moneda de tu país
+	    - Convertir de Won sul-coreano a la moneda de tu país
+		*/
+	
+	public double solAdolar = Math.round((1/DolarASol) * 100.0) / 100.0;
+	public double solAEuro = Math.round((1/EuroASol) * 100.0) / 100.0;
+	public double solALibrasE = Math.round((1/LibrasEASol) * 100.0) / 100.0;
+	public double solAYen = Math.round((1/YenASol) * 100.0) / 100.0;
+	public double solAWon = Math.round((1/WonASol) * 100.0) / 100.0;
+	
 }
