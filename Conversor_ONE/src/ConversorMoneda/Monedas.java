beca833c5358855c4d2505b9d6dcc4e1c6fd1aea
animesmed <k.animesmed27@gmail.com>
2023-08-10 12:32:11 -0500

    Creando la clase que contendra todas las conversiones de las monedas

---
+++ Conversor_ONE/src/ConversorMoneda/Monedas.java
@@ -0,0 +1,49 @@
+package ConversorMoneda;
+
+public class Monedas {
+	/*
+	- Convertir de la moneda de tu país a Dólar
+  	- Convertir de la moneda de tu país  a Euros
+  	- Convertir de la moneda de tu país  a Libras Esterlinas
+  	- Convertir de la moneda de tu país  a Yen Japonés
+  	- Convertir de la moneda de tu país  a Won sul-coreano
+    */
+	public double ConvertirSolADolar(double val) {
+		return val;
+	}
+	public double ConvertirSolAEuro(double val) {
+		return val;
+	}
+	public double ConvertirSolALibrasE(double val) {
+		return val;
+	}
+	public double ConvertirSolAYen(double val) {
+		return val;
+	}
+	public double ConvertirSolAWon(double val) {
+		return val;
+	}
+	//=======
+	/*
+	- Convertir de Dólar a la moneda de tu país
+    - Convertir de Euros a la moneda de tu país
+    - Convertir de Libras Esterlinas a la moneda de tu país
+    - Convertir de Yen Japonés a la moneda de tu país
+    - Convertir de Won sul-coreano a la moneda de tu país
+	*/
+	public double ConvertirDolarASol(double val) {
+		return val;
+	}
+	public double ConvertirEuroASol(double val) {
+		return val;
+	}
+	public double ConvertirLibrasASol(double val) {
+		return val;
+	}
+	public double ConvertirYenASol(double val) {
+		return val;
+	}
+	public double ConvertirWonASol(double val) {
+		return val;
+	}
+}
